a4c38e34eb007ac5dc9ac9e1d3bf36867e494373
Andrey Miroshnichenko <82806873+AmdrewMiroshnichenko@users.noreply.github.com>
2021-05-28 01:43:06 +0300

    Update StringParser.kt

--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -4,6 +4,34 @@ class StringParser {
 
     // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        var counter: Int
+        var temp: Int
+        val result = arrayListOf<String>()
+        val sings = arrayOf('<','>','(',')','[',']')
+
+        for (i in 0..inputString.lastIndex) {
+            for (sign in sings.indices step 2) {
+                when (inputString[i]) {
+                    sings[sign] -> {
+                        counter = 0
+                        temp = i + 1
+                        for (k in i..inputString.lastIndex) {
+                            if (inputString[k] == sings[sign]){
+                                counter += 1
+                            }
+                            if (inputString[k] == sings[sign + 1]) {
+                                counter -= 1
+                                if (counter == 0) {
+                                    result.add(inputString.substring(temp until k))
+                                    break
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return result.toTypedArray()
     }
 }
