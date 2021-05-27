7b772e6f1f5a3a39a436ad445475641473e886d0
Andrey Miroshnichenko <82806873+AmdrewMiroshnichenko@users.noreply.github.com>
2021-05-28 01:39:35 +0300

    Update HappyArray.kt

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -2,8 +2,28 @@ package subtask1
 
 class HappyArray {
 
-    // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        var happyList = sadArray.toMutableList()
+        var isReady = false
+        var index: Int = 1
+        var counter: Int = 0
+
+        while (counter < happyList.size - 1) {
+            while (!isReady && index < happyList.size - 1) {
+                if (happyList.get(index) > happyList.get(index - 1) + happyList.get(index + 1)) {
+                    happyList.removeAt(index)
+                } else {
+                    if (index != happyList.size - 1) {
+                        index++
+                    } else {
+                        isReady = true;
+                    }
+                }
+            }
+            index = 1
+            counter++
+        }
+        val happyArray = happyList.toIntArray()
+        return happyArray
     }
 }
